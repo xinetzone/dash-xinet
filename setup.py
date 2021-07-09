72486b78f1814cd2085204f3cf4da7f9d3330185
xinetzone <xinzone@outlook.com>
2021-07-09 17:31:08 +0800

    new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dash_xinet", 
+    version="0.0.1",
+    author="xinetzone",
+    author_email="xinzone@outlook.com",
+    description="Dash App",
+    long_description=long_description,
+    long_description_content_type="text/markdown", 
+    url="https://github.com/xinetzone/dash-xinet",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 2 - Pre-Alpha",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: POSIX :: Linux"
+    ],
+    python_requires='>=3.9',
+)
